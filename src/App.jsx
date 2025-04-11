a58f7fdf470e13659d58aee1ea1b1868df96c364
Mister D. Ace <ode808prod@gmail.com>
2025-04-12 00:25:28 +0200

    added tabs feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import About from './components/about/About'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <Header />
+     <About />
     </>
   )
 }
