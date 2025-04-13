016695a89d4efba7e8d100cc08bc7b6c28d39ebc
Mister D. Ace <ode808prod@gmail.com>
2025-04-13 03:11:01 +0200

    news structure completed#1

--- src/components/pages/News.jsx
+++ src/components/pages/News.jsx
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import NewsCard from '../news/NewsCard'
 
 const News = () => {
+
+    const [news, setNews] = React.useState([])
+    useEffect(()=>{
+        setNews([
+            {
+                img:'src/assets/news-img.png',
+                title:'Bani ke coder na minuit',
+                date:'Mai 18, 2023',
+                text: 'Ba mboutu hein ? Ba mboutu mu ke safula ngé figure na mbata. Mu tchumuna ngé dissu!'
+            },
+            {
+                img:'src/assets/news-img.png',
+                title:'Ni Beto',
+                date:'Aout 09, 2024',
+                text: 'Ba mboutu hein ? Ba mboutu mu ke safula ngé figure na mbata. Mu tchumuna ngé dissu!'
+            },
+            {
+                img:'src/assets/news-img.png',
+                title:'Ba mbala me ya',
+                date:'Mai 18, 2023',
+                text: 'Ba mboutu hein ? Ba mboutu mu ke safula ngé figure na mbata. Mu tchumuna ngé dissu!'
+            },
+            {
+                img:'src/assets/news-img.png',
+                title:'Himra ke ka zoba',
+                date:'Aout 09, 2024',
+                text: 'Ba mboutu hein ? Ba mboutu mu ke safula ngé figure na mbata. Mu tchumuna ngé dissu!'
+            },
+            {
+                img:'src/assets/news-img.png',
+                title:"L'homme est beau",
+                date:'Mai 18, 2023',
+                text: 'Ba mboutu hein ? Ba mboutu mu ke safula ngé figure na mbata. Mu tchumuna ngé dissu!'
+            },
+            {
+                img:'src/assets/news-img.png',
+                title:'Jonny Ha liiiiii déeee',
+                date:'Aout 09, 2024',
+                text: 'Ba mboutu hein ? Ba mboutu mu ke safula ngé figure na mbata. Mu tchumuna ngé dissu!'
+            },
+            ])
+    }, [])
+
   return (
-    <div>
-      News
-    </div>
+    <section id='news-section'>
+        <h1 className='title2'>News</h1>
+
+        <div className='news-body'>
+        {news.map(item => {
+            return(
+                <NewsCard key={item.title}
+                img={item.img} 
+                title={item.title}
+                date={item.date}
+                text={item.text}
+                
+                />
+            )
+        })}
+        </div>
+
+    </section>
   )
 }
 
