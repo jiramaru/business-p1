73ec2357a6a6ec053ee0f31ebe5e31af99847684
Mister D. Ace <ode808prod@gmail.com>
2025-04-13 06:53:37 +0200

    services section structured

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import About from '../About'
+import Services from '../services/Services'
 
 const Home = () => {
   return (
     <>
     <About/>
+
+    <Services />
     </>
   )
 }
