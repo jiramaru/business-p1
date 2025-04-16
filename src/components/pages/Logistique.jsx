507ec06e3bda0dfde7b939ac4736573a738d15c5
Mister D. Ace <ode808prod@gmail.com>
2025-04-16 11:30:06 +0200

    rse & logistique pages updated

--- src/components/pages/Logistique.jsx
+++ src/components/pages/Logistique.jsx
@@ -5,7 +5,61 @@ const Logistique = () => {
   return (
     <>
       <Hero />
-      <h1>Logistique</h1>
+      <section className='logistique'>
+      
+        <div className="logistique-section">
+
+                    <p className='title2'>Logistique Pétrolière du Congo</p>
+
+              <div className="items">
+                <div className="item">
+                  <img src="src/assets/card.png" class="" />
+                  <p>
+                  La logistique pétrolière au Congo repose sur un processus structuré garantissant l'approvisionnement en carburants à travers le pays. Les produits proviennent soit de la raffinerie CORAF, soit sont importés par les marketeurs à l’aide de navires et barges par voie maritime ou fluviale. Ces produits pétroliers sont réceptionnés et stockés dans les dépôts d'importation de Pointe-Noire et Brazzaville.
+                  </p>
+                </div>
+                <div className="item">
+                  <img src="src/assets/card.png" class="" />
+                  <p>
+                  La SCLOG assure ensuite leur transfert vers les dépôts intérieurs par camions-citernes, barges ou wagons-citernes. Cette organisation logistique permet de desservir efficacement les différentes régions, garantissant un approvisionnement constant, même dans les zones les plus reculées.
+                  </p>
+                </div>
+                <div className="item">
+                  <img src="src/assets/card.png" class="" />
+                  <p>
+                  À la demande de ses clients (marketeurs) tels que SNPC-D, TotalEnergies, Afric, Puma, X-Oil,… la SCLOG charge les produits dans des camions-citernes.  Ces entreprises prennent en charge la distribution finale vers les stations-service, les clients industriels et d'autres infrastructures stratégiques, garantissant ainsi un approvisionnement constant sur tout le territoire.
+                  </p>
+                </div>
+              </div>
+
+        </div>
+
+        <div className="logistique-schema">
+
+          <p>La logistique pétrolière au CONGO</p>
+          <div className="schema"></div>
+          
+        </div>
+
+        <div className="big-carte">
+          
+            <p className='title2'>Réseau de dépôts SCLOG</p>
+            <div className="carte">
+              <div>
+                <img src="src/assets/carte.svg" class="" />
+              </div>
+              <div className='carte-right'>
+                <img src="src/assets/carte-legende.svg" class="" />
+                <div>
+                  <p>Transferts assurés par voie routière</p>
+                  <p>Transferts assurés par voie ferroviaire</p>
+                  <p>Transferts assurés par voie fluviale</p>
+                </div>
+              </div>
+            </div>
+        </div>
+
+    </section>
       
     </>
   )
--- src/components/pages/Rse.jsx
+++ src/components/pages/Rse.jsx
@@ -5,7 +5,80 @@ const Rse = () => {
   return (
     <>
       <Hero />
-      <h1>RSE</h1>
+      <section className="rse">
+
+                <div className="rse1">
+
+                    
+          <p className='title2'>Notre engagement sociétal</p>
+          <p>
+            Chez SCLOG, nous croyons fermement que notre succès va de pair avec notre engagement envers la communauté et l'environnement. Nos initiatives RSE sont conçues pour contribuer positivement au développement durable et au bien-être des populations locales:
+          </p>
+
+
+          <div className="rse1-elements">
+
+            <div className='rse1-element'>
+
+                <div className="rse1-element-img">
+                  <img src="src/assets/service-check.svg" />
+                </div>
+
+                <div className="rse1-element-text">
+                  <p>Education</p>
+                  <p>
+
+                    Réhabilitation et construction de salles de classe à Loutété (2020-2022)
+                    Remise de kits scolaires et inauguration de nouvelles infrastructures à l’école Antoine MILANDOU de Ouesso en 2025.
+                  </p>
+                </div>
+              
+            </div>
+
+            <div className='rse1-element'>
+
+                <div className="rse1-element-img">
+                  <img src="src/assets/service-check.svg"  />
+                </div>
+
+                <div className="rse1-element-text">
+                  <p>Santé</p>
+                  <p>
+
+                    Don de kits pharmaceutiques et de matériel médical à l’hôpital de base d'Impfondo en 2023.
+                    Soutien de causes en faveur de la lutte contre le diabète (Association MCPLC) et la promotion du Sport et de la santé
+                  </p>
+                </div>
+              
+            </div>
+
+            <div className='rse1-element'>
+
+              <div className="rse1-element-img">
+                <img src="src/assets/service-check.svg" />
+              </div>
+
+              <div className="rse1-element-text">
+                <p>Solidarité</p>
+                <p>
+
+                Dons annuels aux hospices et orphelinats, incluant des actions comme la distribution de jouets et de vivres en 2024.
+
+                </p>
+              </div>
+              
+            </div>
+
+          </div>
+
+          <p>
+            Chaque action reflète notre engagement à bâtir un avenir solidaire et responsable.
+          </p>
+
+          </div>
+
+
+      </section>
     </>
   )
 }
